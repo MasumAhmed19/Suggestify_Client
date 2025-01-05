ea5f6bfc0a69e499813fb82e5c323dcefe2e4753
MasumAhmed19 <masum.ahmed1328@gmail.com>
2025-01-05 16:30:52 +0600

    get queries done

--- src/pages/Queries.jsx
+++ src/pages/Queries.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Queries = () => {
+    const [queries, setQueries] = useState([]);
+    
+    useEffect(()=>{
+        fetAllQueries();
+    }, [])
+
+    const fetAllQueries = async ()=>{
+        const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/queries`)
+        setQueries(data)
+    }
+
+    console.log(queries)
     return (
         <div>
-            <h2>Queries</h2>
+            <h2>Queries: {queries.length}</h2>
         </div>
     );
 };
